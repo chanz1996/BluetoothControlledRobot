009a5813bd25da02e07f74b4659807463b0353a9
chandini <chanz1996@users.noreply.github.com>
2021-01-12 20:15:35 +0530

    Add files via upload

---
+++ finalprogram.ino
@@ -0,0 +1,83 @@
+int m1 = 8;
+int m2 = 9;
+int m3 = 10;
+int m4 = 11;
+
+void setup() 
+{
+  // put your setup code here, to run once:
+  pinMode(m1, OUTPUT);
+  pinMode(m2, OUTPUT);
+  pinMode(m3, OUTPUT);
+  pinMode(m4, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+if(Serial.available()>0)
+{
+  char btInput = Serial.read();
+
+  if( btInput == 'f' )
+  {
+    
+    digitalWrite(m1, HIGH);
+    digitalWrite(m2, LOW);
+    digitalWrite(m3, HIGH);
+    digitalWrite(m4, LOW);
+  }
+
+ // delay(500);
+
+  //backward
+  if( btInput == 'b' )
+  {
+    digitalWrite(m1, LOW);
+    digitalWrite(m2, HIGH);
+    digitalWrite(m3, LOW);
+    digitalWrite(m4, HIGH);
+  }
+  
+ // delay(500);
+
+  //left turn
+  if( btInput == 'l' )
+  {
+    digitalWrite(m1, HIGH);
+    digitalWrite(m2, LOW);
+    digitalWrite(m3, LOW);
+    digitalWrite(m4, LOW);
+    delay(5000);
+    digitalWrite(m1, HIGH);
+    digitalWrite(m2, LOW);
+    digitalWrite(m3, HIGH);
+    digitalWrite(m4, LOW);
+    
+  }
+ // delay(500);
+
+  //right turn
+  if( btInput == 'r' )
+  {
+    digitalWrite(m1, LOW);
+    digitalWrite(m2, LOW);
+    digitalWrite(m3, HIGH);
+    digitalWrite(m4, LOW);
+    delay(5000);
+    digitalWrite(m1, HIGH);
+    digitalWrite(m2, LOW);
+    digitalWrite(m3, HIGH);
+    digitalWrite(m4, LOW);
+  }
+  //delay(500);
+
+  if( btInput == 's' )
+ {
+    digitalWrite(m1, LOW);
+    digitalWrite(m2, LOW);
+    digitalWrite(m3, LOW);
+    digitalWrite(m4, LOW);
+  }
+}
+}
